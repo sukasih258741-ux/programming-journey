f822317e4fb8afaddff5f3fbd2d00bb12ed7f884
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-03 14:15:13 +0700

    Add files via upload

---
+++ tugas-6-vinasukasih-xpplg1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_6_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nma = new string[3];
+            char[] jk = new char[3];
+            string[] kelas = new string[3];
+
+            Console.Write("Masukkan nama siswa ke-1: ");
+            nma[0] = Console.ReadLine();
+            Console.Write("Masukkan jenis kelamin (L/P): ");
+            jk[0] = char.Parse(Console.ReadLine());
+            Console.Write("Masukkan kelas: ");
+            kelas[0] = Console.ReadLine();
+
+            Console.Write("Masukkan nama siswa ke-2: ");
+            nma[1] = Console.ReadLine();
+            Console.Write("Masukkan jenis kelamin (L/P): ");
+            jk[1] = char.Parse(Console.ReadLine());
+            Console.Write("Masukkan kelas: ");
+            kelas[1] = Console.ReadLine();
+
+            Console.Write("Masukkan nama siswa ke-3: ");
+            nma[2] = Console.ReadLine();
+            Console.Write("Masukkan jenis kelamin (L/P): ");
+            jk[2] = char.Parse(Console.ReadLine());
+            Console.Write("Masukkan kelas: ");
+            kelas[2] = Console.ReadLine();
+
+            Console.WriteLine("\n=== Data Siswa ===");
+            Console.WriteLine($"Nama: {nma[0]} | Jenis Kelamin: {jk[0]} | Kelas: {kelas[0]}");
+            Console.WriteLine($"Nama: {nma[1]} | Jenis Kelamin: {jk[1]} | Kelas: {kelas[1]}");
+            Console.WriteLine($"Nama: {nma[2]} | Jenis Kelamin: {jk[2]} | Kelas: {kelas[2]}");
+        }
+    }
+}
