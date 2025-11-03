16e5ba7f7b2fa489a72adc3ac213075ba3cfaf6f
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-03 13:53:09 +0700

    Add files via upload

---
+++ praktik7.2_fungsi_vinasukasih_xpplg1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik7._2_fungsi_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+
+        static void SapaNama(string nama)
+        {
+            Console.WriteLine("Halo, " + nama + "!");
+            Console.WriteLine("Semangat belajar C# hari ini!");
+        } 
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nama Anda: ");
+            string namaSiswa = Console.ReadLine();
+            SapaNama(namaSiswa); // Memanggil fungsi dengan argumen
+        }
+    }
+}
