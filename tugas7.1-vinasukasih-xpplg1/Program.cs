a6c8428af7a4f801644454be073449827d64aa50
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-03 13:56:09 +0700

    Add files via upload

---
+++ tugas7.1-vinasukasih-xpplg1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas7._1_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+            static int Perkalian(int a, int b, int c)
+            {
+                return a * b * c;
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("Masukkan angka pertama:");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Masukkan angka kedua:");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Masukkan angka ketiga:");
+                int z = Convert.ToInt32(Console.ReadLine());
+
+                int hasil = Perkalian(x, y, z);
+                Console.WriteLine("Hasil penjumlahan: " + hasil);
+            }
+    }
+}
