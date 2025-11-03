982ec974a4fb330adb5dbebb067af72122d90698
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-03 14:14:07 +0700

    Add files via upload

---
+++ praktik6.2_vinasukasih_xpplg1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._2_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] angka = new int[5];
+            Console.WriteLine("Masukkan nilai ke-1: ");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-2: ");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-3: ");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-4: ");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ke-5: ");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
