758ff903665e8b7c6e494ea409c5386a8a2869b5
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-03 13:54:10 +0700

    Add files via upload

---
+++ praktik7.1_fungsi_vinasukasih_xpplg1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik7._1_fungsi_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil fungsi
+        }
+    }
+}
---
+++ praktik7.1_fungsi_vinasukasih_xpplg1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("praktik7.1_fungsi_vinasukasih_xpplg1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("praktik7.1_fungsi_vinasukasih_xpplg1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e7f8559c-f233-45f3-8f64-5bfa6d6be56c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
