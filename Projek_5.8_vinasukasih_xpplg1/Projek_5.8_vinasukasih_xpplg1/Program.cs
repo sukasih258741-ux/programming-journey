7c8a101faafe7550056341a69f45301a0ea6f29a
sukasih258741-ux <sukasih258741@student.smkn1kandeman.sch.id>
2025-11-24 20:09:21 +0700

    Add files via upload

---
+++ Projek_5.8_vinasukasih_xpplg1/Projek_5.8_vinasukasih_xpplg1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek_5._8_vinasukasih_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses yang Akan Diulang ---
+                Console.WriteLine("----------------------------------");
+                Console.Write("Masukkan nama anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesai.");
+                // -------------------------------------
+                // Pertanyaan untuk Mengulang
+                Console.Write("Apakah Anda ingin mengulang lagi? (ya/tidak): ");
+                ulangiPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian 
+
+                // Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+              Console.WriteLine("Terima kasih, Program selesai.");
+        }
+    }
+}
